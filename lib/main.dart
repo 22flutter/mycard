201049738a81e7ae37b3d62cb90c391e1cfc1033
afraidofhash <abhinpal67@gmail.com>
2022-02-21 02:35:32 +0530

    initial app stage

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,58 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal[100],
+        body: SafeArea(
+          child:Column(
+            /*mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  color: Colors.red,
+                  child: Text('Hello!'),
+                  height: 250,
+                  width: 100,
+                  //margin:EdgeInsets.symmetric(vertical: 50,horizontal: 10)
+              ),
+              Container(
+                  color: Colors.yellow,
+                  child: Text('Hello!'),
+                  height: 250,
+                  width: 100,
+                  //margin:EdgeInsets.symmetric(vertical: 50,horizontal: 10)
+              ),
+              Container(
+                  color: Colors.green,
+                  child: Text('Hello!'),
+                  height: 250,
+                  width: 100,
+                  //margin:EdgeInsets.symmetric(vertical: 50,horizontal: 10)
+              ),
+            ],*/
+
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.deepOrange[700],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
