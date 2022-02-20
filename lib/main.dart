20dfb2da44ebdf914e6fb42f80ab186fa5234175
afraidofhash <abhinpal67@gmail.com>
2022-02-21 04:46:22 +0530

    final app

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal[100],
         body: SafeArea(
           child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             /*mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -45,8 +46,87 @@ class MyApp extends StatelessWidget {
 
             children: <Widget>[
               CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.deepOrange[700],
+                radius: 75,
+                backgroundImage: AssetImage('images/abhi.jpg'),
+                backgroundColor: Colors.deepOrange[600],
+              ),
+              Text('Abhinaba Pal',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'cursive1',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text('Computer Science Student',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'cursive2',
+                  letterSpacing: 3,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+                width: double.infinity,
+              ),
+              SizedBox(
+                height: 5,
+                width: 300.0 ,
+                child: Divider(
+                  color: Colors.teal[900],
+                  thickness: 2,
+                ),
+              ),
+              Card(
+                //padding: EdgeInsets.all(10),
+                margin:EdgeInsets.all(15),
+
+                color:Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 2,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size:50,
+                    ),
+                    title: Text(
+                      '+91 6005764884',
+                      style: TextStyle(
+                        color:Colors.blueGrey[900],
+                        fontFamily: 'cursive2',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  )
+                ),
+              ),
+              Card(
+                //padding: EdgeInsets.all(10),
+                margin:EdgeInsets.all(15),
+
+                color:Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size:50,
+                    ),
+                    title: Text(
+                      '18bcs004@smvdu.ac.in',
+                      style: TextStyle(
+                        color:Colors.blueGrey[900],
+                        fontFamily: 'cursive2',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  )
+                ),
               )
             ],
           ),
@@ -55,5 +135,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 
